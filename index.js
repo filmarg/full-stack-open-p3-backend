dad735beefd3c8f8eb7440e27d7d68ae12a5a7ba
Filip Margielewski <filip.ma@proton.me>
2024-05-30 11:14:42 +0200

    Add exercise 3.4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const persons = [
+let persons = [
   { 
     "id": 1,
     "name": "Arto Hellas", 
@@ -37,6 +37,14 @@ app.get('/api/persons/:id', (req, res) => {
   } else {
     res.json(person)
   }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = +req.params.id
+
+  persons = persons.filter(p => p.id !== id)
+
+  res.status(204).end()
 })
 
 app.get('/info', (req, res) => {
