2982b0c8cec819685eae48dc40654c4fc818587c
Filip Margielewski <filip.ma@proton.me>
2024-05-31 11:27:03 +0200

    Add exercise 3.8

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const morgan = require('morgan')
 
 const app = express()
 
-app.use(morgan('tiny'))
+morgan.token('data', (req) => req.method === 'POST' ? JSON.stringify(req.body) : '')
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 app.use(express.json())
 
 let persons = [
