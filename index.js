738bfc5c7ae4b80667960f2bd9bea38c64c19437
Filip Margielewski <filip.ma@proton.me>
2024-08-02 12:05:26 +0200

    Add exercise 3.17

--- index.js
+++ index.js
@@ -42,6 +42,21 @@ app.delete('/api/persons/:id', (req, res, next) => {
     .catch(err => next(err))
 })
 
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+  
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+  
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(err => next(err))
+})
+
 app.post('/api/persons', (req, res, next) => {
   const body = req.body
   
